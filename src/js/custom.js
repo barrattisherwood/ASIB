bae040c8b9cd20c352f5d65c04a9f3e2314685ac
Barratt Isherwood <b4rr4tt@gmail.com>
2018-08-13 21:21:36 +0200

    refactor(pages nav styling): form and content page, populated nav, back to top

--- src/js/custom.js
+++ src/js/custom.js
@@ -62,7 +62,24 @@ $('.off-canvas-close').click(function(e){
     closeOffCanvasNav();
 });
 
-$('.off-canvas').click(function(){
-    e.stopPropagation();
-});
-
+if ($('#back-to-top').length) {
+    var scrollTrigger = 100, // px
+        backToTop = function () {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop > scrollTrigger) {
+                $('#back-to-top').addClass('show');
+            } else {
+                $('#back-to-top').removeClass('show');
+            }
+        };
+    backToTop();
+    $(window).on('scroll', function () {
+        backToTop();
+    });
+    $('#back-to-top').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
+}
