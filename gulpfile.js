4ec008e37d731bb11b0f2a1cdd5701c1b8edcaac
Barratt Isherwood <b4rr4tt@gmail.com>
2018-08-15 20:24:36 +0200

    refactor(cleanup): reworked gulp to streamline and clean up css, deleted unneccesary files

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
+var minifyCSS   = require('gulp-minify-css');
+
+var autoprefixer = require('gulp-autoprefixer');
+var csso = require('gulp-csso');
+var del = require('del');
+var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+var runSequence = require('run-sequence');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    return gulp.src('src/scss/*.scss')
         .pipe(sass())
+        .pipe(minifyCSS())
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
@@ -27,5 +38,27 @@ gulp.task('serve', ['sass'], function() {
     gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
+
+gulp.task('minify-css', function(){
+    return gulp.src('./src/pre-css/style.css')
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./src/css'))
+});
+
+// Gulp task to minify CSS files
+gulp.task('styles', function () {
+    return gulp.src('./src/sass/styles.scss')
+      // Compile SASS files
+      .pipe(sass({
+        outputStyle: 'nested',
+        precision: 10,
+        includePaths: ['.'],
+        onError: console.error.bind(console, 'Sass error:')
+      }))
+      // Minify the file
+      .pipe(csso())
+      // Output
+      .pipe(gulp.dest('./src/css'))
+  });
 
 gulp.task('default', ['js','serve']);
